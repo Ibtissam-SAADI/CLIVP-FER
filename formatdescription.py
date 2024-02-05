a9de2a40819a4005421447e579128080ff62d00a
Ibtissam Saadi <118973772+Ibtissam-SAADI@users.noreply.github.com>
2024-02-05 12:00:25 +0100

    Update formatdescription.py

--- formatdescription.py
+++ formatdescription.py
@@ -21,45 +21,27 @@ for chemin in [anger_path,disgust_path, fear_path, happy_path, sadness_path, sur
     classe = (chemin.split("\\")[-1])
 
     if classe == "anger":
-        classes.extend(["A facial expression with wide, bright eyes, raised cheeks, a broad smile showing teeth, and relaxed eyebrows."]*len(f_files))
-        #classes.extend(["lowered, furrowed eyebrows, narrowed or glaring eyes, tightly pressed lips, tensed jaw muscles, and a tightened forehead."]*len(f_files))
-        #classes.extend(["Eyebrows down and together, narrowed eyes, tight lips."]*len(f_files))
-        #classes.extend(["furrowed eyebrows, narrow eyes, tightened lips, and flared nostrils."]*len(f_files))
+        classes.extend(["A facial expression with lowered and furrowed eyebrows, narrowed and glaring eyes, flared nostrils, a mouth either firmly pressed or snarling, and a tensed jaw."]*len(f_files))
         all_files.extend(f_files)
         labels.extend([0]*len(f_files))
     elif classe == "disgust":
-        classes.extend(["A facial expression with wide, bright eyes, raised cheeks, a broad smile showing teeth, and relaxed eyebrows."]*len(f_files))
-        #classes.extend(["a scrunched nose, raised upper lip, and squinting eyes, displaying repulsion without the furrowed brow and glare seen in anger."]*len(f_files))
-        #classes.extend(["Wrinkled nose, raised upper lip, squinted eyes."]*len(f_files))
-        #classes.extend(["a wrinkled nose, lowered eyebrows, a tightened mouth, and narrow eyes."]*len(f_files))
+        classes.extend(["A facial expression with a wrinkled nose, raised upper lip, narrowed or squinting eyes, and a slightly open or curled lip."]*len(f_files))
         all_files.extend(f_files)
         labels.extend([1]*len(f_files))
     elif classe == "fear":
-        classes.extend(["A facial expression with wide, bright eyes, raised cheeks, a broad smile showing teeth, and relaxed eyebrows."]*len(f_files))
-        #classes.extend(["wide-open eyes, raised and drawn-together eyebrows, an open mouth, and a stretched, tense forehead, reflecting a sense of alarm or threat."]*len(f_files))
-        #classes.extend(["Wide eyes, raised and drawn together eyebrows, slightly open mouth."]*len(f_files))
-        #classes.extend(["raised eyebrows, parted lips, a furrowed brow, and a retracted chin."]*len(f_files))
+        classes.extend(["A facial expression with wide-open eyes, raised and drawn together eyebrows, a tensed or slightly open mouth, and a generally stretched or elongated face."]*len(f_files))
         all_files.extend(f_files)
         labels.extend([2]*len(f_files))
     elif classe == "happy":
         classes.extend(["A facial expression with wide, bright eyes, raised cheeks, a broad smile showing teeth, and relaxed eyebrows."]*len(f_files))
-        #classes.extend(["a smiling mouth with upturned corners, raised cheeks, and eyes that may crinkle at the corners, often indicating genuine joy or amusement."]*len(f_files))
-        #classes.extend(["Upturned mouth corners, raised cheeks, crinkled eyes."]*len(f_files))
-        #classes.extend(["a smiling mouth, raised cheeks, wrinkled eyes, and arched eyebrows."]*len(f_files))
         all_files.extend(f_files)
         labels.extend([3]*len(f_files))
     elif classe == "sadness":
-        classes.extend(["A facial expression with wide, bright eyes, raised cheeks, a broad smile showing teeth, and relaxed eyebrows."]*len(f_files))
-        #classes.extend(["downturned corners of the mouth, slightly furrowed eyebrows, drooping upper eyelids, and a general downward or softening of facial features, conveying a sense of melancholy or sorrow."]*len(f_files))
-        #classes.extend(["Downturned mouth, drooping eyelids, furrowed brows."]*len(f_files))
-        #classes.extend(["tears, a downward turned mouth, drooping upper eyelids, and a wrinkled forehead."]*len(f_files))
+        classes.extend(["A facial expression with downward pointing corners of the mouth, drooping eyelids, slightly furrowed brows, and a generally downwards and subdued look."]*len(f_files))
         all_files.extend(f_files)
         labels.extend([4]*len(f_files))
     elif classe == "surprise":
-        classes.extend(["A facial expression with wide, bright eyes, raised cheeks, a broad smile showing teeth, and relaxed eyebrows."]*len(f_files))
-        #classes.extend(["raised and curved eyebrows, wide-open eyes, and often a dropped jaw with an open mouth, signifying a sudden shock or astonishment."]*len(f_files))
-        #classes.extend(["Raised eyebrows, wide-open eyes, open mouth."]*len(f_files))
-        #classes.extend(["widened eyes, an open mouth, raised eyebrows, and a frozen expression."]*len(f_files))
+        classes.extend(["A facial expression with raised eyebrows, wide-open eyes, a dropped jaw with the mouth open."]*len(f_files))
         all_files.extend(f_files)
         labels.extend([5]*len(f_files))
 
@@ -70,6 +52,5 @@ for chemin in [anger_path,disgust_path, fear_path, happy_path, sadness_path, sur
             'labels': labels,
         }
     )
-    #dataframe.to_csv('./Descdata.csv', index=False)
-    dataframe.to_csv('./simpledes06.csv', index=False)
+    dataframe.to_csv('./des5data5.csv', index=False)
 print(f"Length of the exported data: {len(dataframe)}")
