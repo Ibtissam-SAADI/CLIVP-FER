da1ce9e90d828ed881272999f7bd9f86424f2333
Ibtissam Saadi <118973772+Ibtissam-SAADI@users.noreply.github.com>
2024-02-07 10:28:54 +0100

    Update 10fold_train.py

--- 10fold_train.py
+++ 10fold_train.py
@@ -1,7 +1,7 @@
 import os
 for i in range(10):
     print('Fold', i)
-    cmd = 'python CLIP_KMU.py --mode 1 --fold %d' %(i+1)
+    cmd = 'python CLIP_KMU.py --model Ourmodel --bs 64 --lr 0.003 --mode 1 --fold %d' %(i+1)
     os.system(cmd)
 print("Train CLIP_KMU ok!")
 os.system('pause')
